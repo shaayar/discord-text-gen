6a4c579e420641f264026c7892c80f675e739d6c
Dev Shubh <shubhidave007@gmail.com>
2025-03-25 16:14:40 +0000

    v1.0

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "discord-text-gen", // Replace with your actual repo name
+  server: {
+    host: true, // Allows access from external URLs
+    strictPort: true, // Ensures it runs on the assigned port
+    allowedHosts: [
+      "shaayar.github.io" // Whitelist GitHub Pages domain
+    ],
+  },
+});
